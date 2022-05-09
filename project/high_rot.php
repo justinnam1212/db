a528c4e2a5b61aa62d1c5a02055db439a34424cc
cammarcus19 <cmarcus9@jhu.edu>
2022-05-08 23:01:08 -0400

    5 and 6 functional

---
+++ project/high_rot.php
@@ -0,0 +1,33 @@
+<head><title>c1: displaying highest rotten scores</title></head>
+<body>
+<?php
+	echo '<body style="background-color:grey">';
+        //open a connection to dbase server
+        include 'open.php';
+
+        echo "<h2>Movie Rating Info</h2><br>";
+	$myQuery = "CALL high_rot();";
+       if ($result = $conn->query($myQuery)) {
+       echo "<table border=\"2px solid black\">";
+       echo "<tr><td> movie ID </td><td> movie name </td><td> tomatometer rating (out of 100)  </td></tr>";
+   
+       foreach($result as $row){
+             echo "<tr>";
+             echo "<td>".$row['movieID']."</td>";
+             echo "<td>".$row['movie_name']."</td>";
+             echo "<td>".$row['tomatometer_rating']."</td>";
+             echo "</tr>";
+          }
+              echo "</table>";
+       } else {
+       
+          echo "Call to movie_rating failed<br>";
+       }
+       
+       // } else {
+         //  echo "not set";
+       // }
+        $conn->close();
+	
+?>
+</body>
---
+++ project/high_rot.sql
@@ -0,0 +1,24 @@
+-- high_rot.sql
+-- displays info for each rated movie
+
+DELIMITER ?
+
+DROP PROCEDURE IF EXISTS high_rot ?
+
+CREATE PROCEDURE high_rot()
+BEGIN
+
+
+	SELECT DISTINCT movieID, movie_name, tomatometer_rating
+       FROM Review
+     WHERE tomatometer_status = 'Rotten'
+      ORDER BY tomatometer_rating DESC
+      LIMIT 10;
+
+
+END; ?
+
+DELIMITER ;
+
+
+
