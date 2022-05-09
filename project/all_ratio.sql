1d59e128e70e2f2d96f7fc5898abe2ef5a5a4119
cammarcus19 <cmarcus9@jhu.edu>
2022-05-09 01:21:02 -0400

    all_ratio functional

---
+++ project/all_ratio.sql
@@ -0,0 +1,23 @@
+-- all_ratio.sql
+-- Displays highest and lowest ratios for all movies                                 
+
+DELIMITER ?
+
+DROP PROCEDURE IF EXISTS all_ratio ?
+
+CREATE PROCEDURE all_ratio()
+BEGIN
+
+SELECT movie, 100 * domestic_box_office / international_box_office AS box_office_ratio
+FROM Movie
+ORDER BY box_office_ratio DESC
+LIMIT 10;
+
+SELECT movie, 100 * domestic_box_office / international_box_office AS box_office_ratio
+FROM Movie
+ORDER BY box_office_ratio ASC
+LIMIT 10;
+
+END; ?
+
+DELIMITER ;
