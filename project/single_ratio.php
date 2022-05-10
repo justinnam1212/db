5e2332038c97739682f61c391c27aa8308a36969
cammarcus19 <cmarcus9@jhu.edu>
2022-05-10 02:16:51 -0400

    pie chart for single_ratio added

--- project/single_ratio.php
+++ project/single_ratio.php
@@ -21,7 +21,13 @@
        echo "box office ratio is the percentage of box office that is domestic (100% * domestic/international) <br><br>";
        echo "<table border=\"2px solid black\">";
        echo "<tr><td> movie </td><td> box office ratio </td></tr>";
-          foreach($result as $row){
+       $dataPoints = array();
+       foreach($result as $row){
+       		       $domestic = $row["box_office_ratio"];
+           }
+array_push($dataPoints, array( "label"=> "% domestic", "y"=> $domestic));	   
+array_push($dataPoints,	array( "label" => "% international minus domestic","y" => 100-$domestic));
+       foreach($result as $row){
              echo "<tr>";
              echo "<td>".$row["movie"]."</td>";
              echo "<td>".$row["box_office_ratio"]."</td>";
@@ -38,3 +44,31 @@
 
 ?>
 </body>
+
+
+
+<html>
+<head>
+<script>
+window.onload = function () {
+        var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                exportEnabled: true,
+                theme: "light1", // "light1", "light2", "dark1", "dark2"
+                title:{
+                        text: "Domestic vs International Box Office"
+			},
+                data: [{
+                        type: "pie", //change type to column, bar, line, area, pie, etc
+                        dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+                }]
+        });
+        chart.render();
+}
+</script>
+</head>
+<body>
+        <div id="chartContainer" style="height: 400px; width: 100%;"></div>
+        <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
